0d8a157bfc792acc60bfce17ae208f1ee085701e
ummanmemmedov2005@gmail.com <ummanmm@code.edu.az>
2024-02-08 11:07:56 +0400

    Files added

---
+++ ConsoleApp37/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37 {
+    internal class Person {
+        public string? Fullname { get; set; }
+        public byte Age { get; set; }
+
+        public Person(string? fullname, byte age) {
+            Fullname = fullname;
+            Age = age;
+        }
+
+        override public string ToString() {
+            return $"Fullname: {Fullname}, Age: {Age}";
+        }
+    }
+}
---
+++ ConsoleApp37/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using ConsoleApp37;
+
+Person person1 = new("John Doe", 25);
+
+string? filePath = "C:\\Users\\user\\source\\repos\\ConsoleApp37\\ConsoleApp37\\Person.json";
+
+using (FileStream fs = new (filePath, FileMode.Create)) {
+    JsonSerializer.SerializeAsync(fs, person1).Wait();
+}
+
+Console.WriteLine("Data has been written to file successfully.");
+
+using (FileStream fs = new(filePath, FileMode.Open)) {
+    Person? person2 = JsonSerializer.DeserializeAsync<Person>(fs).Result;
+    Console.WriteLine(person2);
+}
+
+List<Person> personList = new ();
+
+string? listFilePath = "C:\\Users\\user\\source\\repos\\ConsoleApp37\\ConsoleApp37\\PersonList.json";
+
+using (FileStream fs = new(listFilePath, FileMode.Create)) {
+    JsonSerializer.SerializeAsync(fs, personList).Wait();
+}
+
+string? option;
+do {
+    Console.WriteLine("1. Add a person");
+    Console.WriteLine("2. View all persons");
+    Console.WriteLine("0. Exit");
+    Console.Write("Please choose an option: ");
+    option = Console.ReadLine();
+
+    switch (option) {
+        case "1":
+            Console.Write("Enter fullname: ");
+            string? fullname = Console.ReadLine();
+            byte age;
+            do {
+                Console.Write("Enter age: ");
+            } while (!byte.TryParse(Console.ReadLine(), out age));
+            using (FileStream fs = new(listFilePath, FileMode.Open)) {
+                personList = JsonSerializer.DeserializeAsync<List<Person>>(fs).Result;
+                personList.Add(new Person(fullname, age));
+            }
+            using (FileStream fs = new(listFilePath, FileMode.Create)) {
+                JsonSerializer.SerializeAsync(fs, personList).Wait();
+            }
+            break;
+        case "2":
+            using (FileStream fs = new(listFilePath, FileMode.Open)) {
+                personList = JsonSerializer.DeserializeAsync<List<Person>>(fs).Result;
+                foreach (Person person in personList) {
+                    Console.WriteLine(person);
+                }
+            }
+            break;
+        case "0":
+            Console.WriteLine("Good Bye!");
+            break;
+        default:
+            Console.WriteLine("Invalid option.");
+            break;
+    }
+} while (option != "0");
+
